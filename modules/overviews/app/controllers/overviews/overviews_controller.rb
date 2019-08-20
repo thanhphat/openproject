1bbdd52de7eb92f1540b62ffc617c8532f3ce0b6
ulferts <jens.ulferts@googlemail.com>
2019-08-20 08:41:27 +0200

    extract grid controller into base class

--- modules/overviews/app/controllers/overviews/overviews_controller.rb
+++ modules/overviews/app/controllers/overviews/overviews_controller.rb
@@ -1,7 +1,5 @@
 module ::Overviews
-  class OverviewsController < ::ApplicationController
-    before_action :find_optional_project
-    before_action :authorize
+  class OverviewsController < ::Grids::BaseInProjectController
     before_action :jump_to_project_menu_item
 
     menu_item :overview
@@ -11,10 +9,6 @@ module ::Overviews
         # try to redirect to the requested menu item
         redirect_to_project_menu_item(@project, params[:jump]) && return
       end
-    end
-
-    def show
-      render layout: 'angular'
     end
   end
 end
